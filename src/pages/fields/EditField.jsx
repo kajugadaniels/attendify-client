35728841179c4880ea2c16d8c82188f126bc9d91
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-24 15:45:49 +0200

    Add files for field crud

---
+++ src/pages/fields/EditField.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditField = () => {
+    return (
+        <div>EditField</div>
+    )
+}
+
+export default EditField
---
+++ src/pages/fields/GetFields.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetFields = () => {
+    return (
+        <div>GetFields</div>
+    )
+}
+
+export default GetFields

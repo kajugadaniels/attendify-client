ba4e5beca285f8c6bda6b9a29eaf3db6aa71ab27
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-24 13:34:34 +0200

    Setup user CRUD pages

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dashboard, Login, Profile } from './pages'
+import { AddUser, Dashboard, EditUser, GetUsers, Login, Profile } from './pages'
 import UserLayout from './layouts/UserLayout'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,7 +9,12 @@ const AppRoutes = () => {
             <Route path="/" element={<Login />} />
             <Route element={<UserLayout />}>
                 <Route path="/dashboard" element={<Dashboard />} />
+
                 <Route path="/profile" element={<Profile />} />
+
+                <Route path="/users" element={<GetUsers />} />
+                <Route path="/user/add" element={<AddUser />} />
+                <Route path="/user/edit/:id" element={<EditUser />} />
             </Route>
         </Routes>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,7 @@ export { default as Login } from './Login'
 export { default as Dashboard } from './Dashboard'
 
 export { default as Profile } from './Profile'
+
+export { default as GetUsers } from './users/GetUsers'
+export { default as AddUser } from './users/AddUser'
+export { default as EditUser } from './users/EditUser'

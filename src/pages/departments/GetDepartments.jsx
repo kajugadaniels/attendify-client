b164b8c712d9dc5cbc77b91f96333fd01483f96e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-03 18:29:32 +0200

    Add Departments pages

---
+++ src/pages/departments/GetDepartments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetDepartments = () => {
+    return (
+        <div>GetDepartments</div>
+    )
+}
+
+export default GetDepartments
---
+++ src/pages/departments/ShowDepartment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowDepartment = () => {
+    return (
+        <div>ShowDepartment</div>
+    )
+}
+
+export default ShowDepartment
